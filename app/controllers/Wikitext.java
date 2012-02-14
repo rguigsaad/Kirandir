31705ddae9a359f04879a017d19acbf2a9c61574
Arnaud Bovin <arno94600@hotmail.com>
2012-02-14 23:58:31 +0100

    Ajout du Rich Text Editor
    Signed-off-by: Arnaud Bovin <arno94600@hotmail.com>

---
+++ app/controllers/Wikitext.java
@@ -0,0 +1,47 @@
+package controllers;
+ 
+import java.io.StringWriter;
+ 
+import jj.play.org.eclipse.mylyn.wikitext.core.parser.MarkupParser;
+import jj.play.org.eclipse.mylyn.wikitext.core.parser.builder.HtmlDocumentBuilder;
+import jj.play.org.eclipse.mylyn.wikitext.core.parser.builder.HtmlDocumentBuilder.Stylesheet;
+import jj.play.org.eclipse.mylyn.wikitext.textile.core.TextileLanguage;
+import play.mvc.Controller;
+ 
+public class Wikitext extends Controller {
+ 
+    // Controller classique Play! qui sera appelé par l'iframe Preview de MarkItUp.
+    public static void renderPreview(String wiki) {
+        StringWriter writer = new StringWriter();
+        HtmlDocumentBuilder builder = new HtmlDocumentBuilder(writer);
+ 
+        // Ajout de mes 2 css.
+        Stylesheet css = new Stylesheet("/public/stylesheets/main.css");
+        builder.addCssStylesheet(css);
+        css = new Stylesheet("/public/stylesheets/wiki.css");
+        builder.addCssStylesheet(css);
+ 
+        // Création du parser Textile
+        MarkupParser parser = new MarkupParser(new TextileLanguage());
+        parser.setBuilder(builder);
+        parser.parse(wiki);
+ 
+        String htmlContent = writer.toString();
+        renderText(htmlContent);
+    }
+ 
+    // Méthode qui sera appelée par les templates Groovy de Play!.
+    public static String render(String wiki) {
+        StringWriter writer = new StringWriter();
+        HtmlDocumentBuilder builder = new HtmlDocumentBuilder(writer);
+        // Empêche la génération des balises html et body.
+        builder.setEmitAsDocument(false);
+ 
+        // Création du parser Textile
+        MarkupParser parser = new MarkupParser(new TextileLanguage());
+        parser.setBuilder(builder);
+        parser.parse(wiki);
+ 
+        return writer.toString();
+    }
+}
